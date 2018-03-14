cfd4533c6ed6128b75da700be87c1805726b910c
L <trgree@foxmail.com>
2018-03-15 06:43:43 +0800

    add proxy

--- strategy/src/main/java/org/ace/headfirst/Demo.java
+++ strategy/src/main/java/org/ace/headfirst/Demo.java
@@ -3,7 +3,7 @@ package org.ace.headfirst;
 /**
  * Created by Liangsj on 2018/3/12.
  */
-public class Deom {
+public class Demo {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.performFly();
